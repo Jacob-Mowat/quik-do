6ed24968d388d75c0de42b45b31372bbb6b4003e
Jacob-Mowat <zaxquit@protonmail.com>
2023-12-11 03:01:34 +0000

    [Feat] Created Header component

---
+++ client/src/components/header.component.tsx
@@ -0,0 +1,24 @@
+const Header = () => {
+
+    return (
+        <header className='h-20 flex items-center justify-between px-8 bg-[#0C0D0A]'>
+            <div className='flex items-center'>
+                <span className="text-2xl font-bold text-[#D9D3C7]">QuikDo</span>
+            </div>
+
+            {/* Menu actions */}
+            <nav className="flex items-center space-x-4">
+                <a href="#" className="hover:text-[#BFB854] text-[#D9D3C7]">Today</a>
+                <a href="#" className="hover:text-[#BFB854] text-[#D9D3C7]">This Week</a>
+                <a href="#" className="hover:text-[#BFB854] text-[#D9D3C7]">This Month</a>
+            </nav>
+
+            <nav className="flex items-center space-x-4">
+                <a href="#" className="hover:text-[#BFB854] text-[#D9D3C7]">Login</a>
+                <a href="#" className="hover:text-[#BFB854] text-[#D9D3C7]">Register</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
