3837ebdab2bc40ebfa3485b1b3fbfeadec255e19
Jacob-Mowat <zaxquit@protonmail.com>
2024-01-29 04:19:09 +0000

    [Fix] Removed unused imports and fixed a few HTML bugs with the JSX elements

--- client/src/components/header.component.tsx
+++ client/src/components/header.component.tsx
@@ -1,24 +1,17 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect, useState } from "react";
 import { TodoDateFilters, TodoFilterDispatchActions, useTodoFilter, useTodoFilterDispatch } from "../contexts/TodoFilter.context";
 
 const Header = () => {
-    const { isAuthenticated, logout, loginWithRedirect, isLoading, user } = useAuth0();
+    const { isAuthenticated, logout, loginWithRedirect, isLoading } = useAuth0();
     const dispatch = useTodoFilterDispatch();
     const selectedFilter = useTodoFilter();
-    
+
     const dates = [
         TodoDateFilters.ALL,
         TodoDateFilters.TODAY,
         TodoDateFilters.LAST_7_DAYS,
         TodoDateFilters.LAST_30_DAYS,
     ];
-
-    useEffect(() => {
-        if (isAuthenticated) {
-            console.log(user);
-        }
-    }, [isAuthenticated]);
 
     if (isLoading) {
         return <div>Loading...</div>;
@@ -34,16 +27,16 @@ const Header = () => {
             <nav className="flex items-center space-x-4">
                 {/* Filter todos */}
                 {isAuthenticated && dates.map((filterOption) => (
-                    <a key={filterOption} href="#" onClick={(e) => dispatch({ type: TodoFilterDispatchActions.SET_FILTER, payload: { filter: filterOption}})} className={`hover:text-[#BFB854] ${selectedFilter === filterOption ? "text-xl text-[#BFB854]" : "text-[#D9D3C7]"}`}>{filterOption}</a>
+                    <button key={filterOption} onClick={(e: any) => { e.preventDefault(); dispatch({ type: TodoFilterDispatchActions.SET_FILTER, payload: { filter: filterOption } }); }} className={`hover:text-[#BFB854] ${selectedFilter === filterOption ? "text-xl text-[#BFB854]" : "text-[#D9D3C7]"}`}>{filterOption}</button>
                 ))}
             </nav>
 
             <nav className="flex items-center space-x-4">
                 {isAuthenticated && (
-                <a href="#" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })} className="hover:text-[#BFB854] text-[#D9D3C7]">Logout</a>
+                <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })} className="hover:text-[#BFB854] text-[#D9D3C7]">Logout</button>
                 )}
                 {!isAuthenticated && (
-                <a href="#" onClick={() => loginWithRedirect() }className="hover:text-[#BFB854] text-[#D9D3C7]">Login</a>
+                <button onClick={() => loginWithRedirect() }className="hover:text-[#BFB854] text-[#D9D3C7]">Login</button>
                 )}
             </nav>
         </header>
